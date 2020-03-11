6c04a02a87032677dc959d76296fbdc9006c0b84
Hugo BARTHELEMY <19645538+hugobarthelemy@users.noreply.github.com>
2020-03-11 18:29:49 +0100

    Feature: Rubocop check if low queue is specify (#16)
    * Rubocop check if low queue is specify
    
    * Build klaxit-ruby gem

---
+++ rubocop-klaxit/cops/specify_low_queue_in_migration.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    # Cop that makes sure low queue is specify in worker call.
+    class SpecifyLowQueueInMigration < RuboCop::Cop::Cop
+      MSG = "Specify low queue when you call worker in migration with " \
+            "'HardWorker.set(queue: :app_name_low).perform_async(args)' " \
+            "or MyClass.delay(queue: 'app_name_low').some_method(1, 2, 3)"
+
+      def on_send(node)
+        return nil unless in_migration?(node)
+        return unless sidekiq_called?(node)
+        return if low_queue_specify?(node)
+
+        add_offense(node.arguments.first, location: :expression)
+      end
+
+      def sidekiq_called?(node)
+        worker_called?(node) ||
+          sidekiq_delay?(node) ||
+          sidekiq_client_called?(node)
+      end
+
+      def low_queue_specify?(node)
+        low_queue_specify_for_perform?(node) ||
+          low_queue_specify_for_delay?(node) ||
+          low_queue_specify_for_push?(node)
+      end
+
+      def in_migration?(node)
+        dirname = File.dirname(node.location.expression.source_buffer.name)
+        dirname.end_with?("db/migrate")
+      end
+
+      def_node_matcher :worker_called?, <<~PATTERN
+        (send (...) {:perform_async :perform_in :perform_at} ...)
+      PATTERN
+
+      def_node_matcher :sidekiq_delay?, <<~PATTERN
+        (send (send (...) {:delay :delay_for :delay_until} ...) ... (...))
+      PATTERN
+
+      def_node_matcher :sidekiq_push?, <<~PATTERN
+        (send (const (const nil? :Sidekiq) :Client) :push ...)
+      PATTERN
+
+      def_node_matcher :sidekiq_client_called?, <<~PATTERN
+        (send (const (const nil? :Sidekiq) :Client) :push ...)
+      PATTERN
+
+      def_node_matcher :low_queue_specify_for_perform?, <<~PATTERN
+        (send (send (...) :set (hash (pair (sym :queue) (...))))
+        {:perform_async :perform_in :perform_at} ...)
+      PATTERN
+
+      def_node_matcher :low_queue_specify_for_delay?, <<~PATTERN
+        (send (send (...) {:delay :delay_for :delay_until}
+        (hash (pair (sym :queue) (...)))) ... (...))
+      PATTERN
+
+      def_node_matcher :low_queue_specify_for_push?, <<~PATTERN
+        (send (const (const nil? :Sidekiq) :Client) :push
+        (hash (pair (str "class") (...))
+              (pair (str "args") (...))
+              (pair (str "queue") (...)))
+        )
+      PATTERN
+    end
+  end
+end
---
+++ rubocop-klaxit/rubocop.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+require_relative "cops/specify_low_queue_in_migration"
