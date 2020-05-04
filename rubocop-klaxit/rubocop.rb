59e3ffa2444c1ad3bca9a4b949e6774e05085924
Quentin Wentzler <quentin.wentzler@klaxit.com>
2020-05-04 10:37:16 +0200

    add cop to check presence of tag @path

--- rubocop-klaxit/rubocop.rb
+++ rubocop-klaxit/rubocop.rb
@@ -3,3 +3,4 @@
 require_relative "cops/specify_low_queue_in_migration"
 require_relative "cops/active-record/update_attribute"
 require_relative "cops/active-record/save_bang"
+require_relative "cops/check_path_documented"
