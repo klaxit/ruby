4d3a4ad23ed9eb74a60012a6901231308a991633
Antoine Girard <antoine.girard@klaxit.com>
2021-07-28 12:14:54 +0200

    Fix comments and msg

--- rubocop-klaxit/cops/postgres_no_between.rb
+++ rubocop-klaxit/cops/postgres_no_between.rb
@@ -3,10 +3,11 @@
 module RuboCop
   module Cop
     module PostgreSQL
-      # Using BETWEEN in PostgreSQL is a bad practice. We a cop to detect
-      # between keyword in string.
+      # Using BETWEEN in PostgreSQL is a bad practice. We add a cop to detect
+      # between keyword in string. It may give false positives.
+      # See https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_BETWEEN_.28especially_with_timestamps.29
       class NoBetween < Cop
-        MSG = "Do NOT use BETWEEN in PostgreSQL. Please refer: "\
+        MSG = "Do NOT use BETWEEN in PostgreSQL. Please refer to: "\
               "https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_BETWEEN_.28especially_with_timestamps.29"
 
         # Add offense when detect between in a string
