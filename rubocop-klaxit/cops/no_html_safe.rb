b66b20ce45c9abf249bccb1d911da65a4ad90df8
teckwan <wong.teck.wan@outlook.com>
2022-10-21 10:55:18 +0200

    Add tests

--- rubocop-klaxit/cops/no_html_safe.rb
+++ rubocop-klaxit/cops/no_html_safe.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Cop
     # Forbid usage of html_safe which can introduce XSS vulnerabilities
     class NoHtmlSafe < Cop
-      MSG = "Prefer `sanitize` or `strip_tags` over usage of `html_safe`."
+      MSG = "Prefer `sanitize` or `strip_tags` over usage of `html_safe`"
       RESTRICT_ON_SEND = %i(html_safe).freeze
 
       def on_send(node)
