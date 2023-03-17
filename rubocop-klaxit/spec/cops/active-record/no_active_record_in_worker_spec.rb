a27e0302c2ac1f8b64cb7d95fb1f7dbb17b7e1c5
clemkeirua <clement@keiruaprod.fr>
2023-03-17 09:28:05 +0100

    fix test

--- rubocop-klaxit/spec/cops/active-record/no_active_record_in_worker_spec.rb
+++ rubocop-klaxit/spec/cops/active-record/no_active_record_in_worker_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe RuboCop::Cop::ActiveRecord::NoActiveRecordInWorker, :config do
   describe "when we call call_async on a service" do
     context "when the service has an instance of an ActiveRecord object" do
       before do
-        allow(cop).to receive(:is_service_blacklisted?)
-          .with("ZendeskCreateTicketBad")
-          .and_return(true)
+
       end
       it "registers an offense" do
+        expect(cop).to receive(:is_service_blacklisted?)
+                         .with(:ZendeskCreateTicketBad)
+                         .and_return(true)
         expect_offense(<<~RUBY)
           ZendeskCreateTicketBad.call_async(mate)
         RUBY
@@ -17,11 +18,12 @@ RSpec.describe RuboCop::Cop::ActiveRecord::NoActiveRecordInWorker, :config do
 
     context "when the service uses identifiers" do
       before do
-        allow(cop).to receive(:is_service_blacklisted?)
-          .with("ZendeskCreateTicketBad")
-          .and_return(true)
+
       end
       it "does not register an offense" do
+        expect(cop).to receive(:is_service_blacklisted?)
+                         .with(:ZendeskCreateTicketGood)
+                         .and_return(false)
         expect_no_offenses(<<~RUBY)
           ZendeskCreateTicketGood.call_async(123)
         RUBY
