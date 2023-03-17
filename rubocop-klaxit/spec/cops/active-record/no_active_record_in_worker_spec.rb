1b223cb6254e1ea634dbff392af75a57655145ce
clemkeirua <clement@keiruaprod.fr>
2023-03-17 09:28:35 +0100

    fix test

--- rubocop-klaxit/spec/cops/active-record/no_active_record_in_worker_spec.rb
+++ rubocop-klaxit/spec/cops/active-record/no_active_record_in_worker_spec.rb
@@ -3,9 +3,6 @@
 RSpec.describe RuboCop::Cop::ActiveRecord::NoActiveRecordInWorker, :config do
   describe "when we call call_async on a service" do
     context "when the service has an instance of an ActiveRecord object" do
-      before do
-
-      end
       it "registers an offense" do
         expect(cop).to receive(:is_service_blacklisted?)
                          .with(:ZendeskCreateTicketBad)
@@ -17,9 +14,6 @@ RSpec.describe RuboCop::Cop::ActiveRecord::NoActiveRecordInWorker, :config do
     end
 
     context "when the service uses identifiers" do
-      before do
-
-      end
       it "does not register an offense" do
         expect(cop).to receive(:is_service_blacklisted?)
                          .with(:ZendeskCreateTicketGood)
